2c9b72dbc66288cb55861dc688b59e56bceda012
Fredy Orlando Fernández Espinoza <orl_gem18@hotmail.com>
2026-09-08 17:10:35 -0500

    feat: API Node.js con Sequelize

--- config/db.js
+++ config/db.js
@@ -2,13 +2,22 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || 'tienda_db',
-  process.env.DB_USER || 'root',
-  process.env.DB_PASSWORD || '',
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // Asegúrate de incluir el puerto aquí
     dialect: 'mysql',
-    logging: false
+    logging: false,
+    // AGREGA ESTE BLOQUE DIALECTOPTIONS PARA AIVEN:
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false // Permite conectar de forma segura sin descargar el archivo .pem físicamente
+      }
+    }
   }
 );
+
 module.exports = sequelize;
